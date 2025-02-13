e45162c6c4ad3c72eb98c529438880b3c2ba97c1
josemanuel494 <josemanuel.martinc@edu.uah.es>
2025-02-13 09:41:01 +0100

    Update sudoku.pl

--- sudoku.pl
+++ sudoku.pl
@@ -1,3 +1,31 @@
+% Representación de un Sudoku
+sudoku([
+    '.', '.', 3, '.', 2, '.', 7, '.', '.',
+    5, '.', '.', '.', '.', '.', 4, '.', 3,
+    '.', '.', 3, '.', '.', '.', 2, 5, '.',
+    '.', 5, '.', 1, '.', 6, '.', '.', '.',
+    '.', '.', 4, 8, '.', 7, '.', '.', '.',
+    2, 3, 7, 6, '.', 4, 8, '.', '.',
+    '.', 8, '.', '.', '.', 2, '.', 7, '.',
+    3, '.', '.', 4, '.', '.', 2, '.', 8,
+    '.', '.', 9, '.', '.', '.', '.', 6, '.'
+]).
+% REGLAS DE EJECUCIÓN.
+% sudoku(S), cuadrante(S,0,P).
+% sudoku(S), fila(S, 2, Fila).
+% Mostrar el Sudoku en formato 9x9
+mostrar_sudoku([]) :- nl.
+mostrar_sudoku(Sudoku) :-
+    mostrar_filas(Sudoku, 1).
+
+mostrar_filas([], _).
+mostrar_filas(Sudoku, Fila) :-
+    length(FilaActual, 9), % Tomar 9 elementos
+    append(FilaActual, Resto, Sudoku),
+    write(FilaActual), nl,
+    SiguienteFila is Fila + 1,
+    mostrar_filas(Resto, SiguienteFila).
+
 % DEVOLVER FILA DE SUDOKU
 fila([A, B, C, D, E, F, G, H, I |_], 0, [A, B, C, D, E, F, G, H , I]).
 fila([_, _, _, _, _, _, _, _, _ | Cola], Pos, Fila) :-
@@ -28,3 +56,35 @@ unir(X,Y,L) :-
     append(X,Y,ZR),
     eliminar_repetidos(ZR,Z),
     sort(Z,L).
+
+cuadrante(Sudoku, NumCuadrante, Elementos) :-
+    % Obtener fila y columna base
+    FilaBase is (NumCuadrante // 3) * 3,
+    ColBase is (NumCuadrante mod 3) * 3,
+    
+    % Calcular índice base
+    Base is (FilaBase * 9) + ColBase,
+    
+    % Calcular todos los índices necesarios
+    I1 is Base,
+    I2 is Base + 1,
+    I3 is Base + 2,
+    I4 is Base + 9,
+    I5 is Base + 10,
+    I6 is Base + 11,
+    I7 is Base + 18,
+    I8 is Base + 19,
+    I9 is Base + 20,
+    
+    % Extraer elementos usando índices calculados
+    nth0(I1, Sudoku, E1),
+    nth0(I2, Sudoku, E2),
+    nth0(I3, Sudoku, E3),
+    nth0(I4, Sudoku, E4),
+    nth0(I5, Sudoku, E5),
+    nth0(I6, Sudoku, E6),
+    nth0(I7, Sudoku, E7),
+    nth0(I8, Sudoku, E8),
+    nth0(I9, Sudoku, E9),
+    
+    Elementos = [E1, E2, E3, E4, E5, E6, E7, E8, E9].
