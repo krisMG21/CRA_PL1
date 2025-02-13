d9297eabf279c3adbc009256ca3a616b44bbc923
krisMG21 <cristimg9@gmail.com>
2025-02-13 10:18:25 +0100

    Codigo ordenado

--- sudoku.pl
+++ sudoku.pl
@@ -1,18 +1,20 @@
 % Representación de un Sudoku
 sudoku([
-    '.', '.', 3, '.', 2, '.', 7, '.', '.',
-    5, '.', '.', '.', '.', '.', 4, '.', 3,
-    '.', '.', 3, '.', '.', '.', 2, 5, '.',
-    '.', 5, '.', 1, '.', 6, '.', '.', '.',
-    '.', '.', 4, 8, '.', 7, '.', '.', '.',
-    2, 3, 7, 6, '.', 4, 8, '.', '.',
-    '.', 8, '.', '.', '.', 2, '.', 7, '.',
-    3, '.', '.', 4, '.', '.', 2, '.', 8,
-    '.', '.', 9, '.', '.', '.', '.', 6, '.'
+    '.','.', 6 ,'.', 5 , 7 , 8 , 2 ,'.',
+     2 , 1 ,'.','.','.', 6 , 7 , 5 ,'.',
+     7 , 5 , 3 ,'.','.', 4 ,'.','.', 9 ,
+    '.', 6 ,'.','.','.', 5 , 3 ,'.','.',
+    '.','.','.', 7 ,'.','.', 4 ,'.', 2 ,
+     3 ,'.','.', 9 , 2 , 8 ,'.','.', 6 ,
+    '.', 2 ,'.', 5 , 4 ,'.','.','.', 1 ,
+    '.','.', 1 ,'.', 7 ,'.', 9 , 4 , 8 ,
+    '.','.', 9 ,'.','.', 1 ,'.', 7 ,'.',
+     
 ]).
 % REGLAS DE EJECUCIÓN.
-% sudoku(S), cuadrante(S,0,P).
-% sudoku(S), fila(S, 2, Fila).
+% ?- sudoku(S), cuadrante(S,0,P).
+% ?- sudoku(S), fila(S, 2, Fila).
+
 % Mostrar el Sudoku en formato 9x9
 mostrar_sudoku([]) :- nl.
 mostrar_sudoku(Sudoku) :-
@@ -43,20 +45,7 @@ columna_aux(Matriz, Indice, Restantes, Acumulador, Columna) :-
     R is Restantes - 1,
     columna_aux(Matriz, I, R, [Elemento|Acumulador], Columna).
 
-% UNIR POSIBILIDADES
-eliminar_repetidos([], []).
-eliminar_repetidos([H|T], R) :-
-    member(H, T), % Si H está en el resto de la lista, lo ignoramos
-    eliminar_repetidos(T, R).
-eliminar_repetidos([H|T], [H|R]) :-
-    \+ member(H, T), % Si H no está en el resto de la lista, lo mantenemos
-    eliminar_repetidos(T, R).
-
-unir(X,Y,L) :-
-    append(X,Y,ZR),
-    eliminar_repetidos(ZR,Z),
-    sort(Z,L).
-
+% DEVOLVER CUADRANTE DE SUDOKU
 cuadrante(Sudoku, NumCuadrante, Elementos) :-
     % Obtener fila y columna base
     FilaBase is (NumCuadrante // 3) * 3,
@@ -71,8 +60,22 @@ cuadrante(Sudoku, NumCuadrante, Elementos) :-
     I7 is Base + 18, I8 is Base + 19,    I9 is Base + 20,
     
     % Extraer elementos usando índices calculados
-    nth0(I1, Sudoku, E1),    nth0(I2, Sudoku, E2),     nth0(I3, Sudoku, E3),
-    nth0(I4, Sudoku, E4),    nth0(I5, Sudoku, E5),     nth0(I6, Sudoku, E6),
-    nth0(I7, Sudoku, E7),    nth0(I8, Sudoku, E8),     nth0(I9, Sudoku, E9),
+    nth0(I1, Sudoku, E1),    nth0(I2, Sudoku, E2),    nth0(I3, Sudoku, E3),
+    nth0(I4, Sudoku, E4),    nth0(I5, Sudoku, E5),    nth0(I6, Sudoku, E6),
+    nth0(I7, Sudoku, E7),    nth0(I8, Sudoku, E8),    nth0(I9, Sudoku, E9),
     
     Elementos = [E1, E2, E3, E4, E5, E6, E7, E8, E9].
+
+% UNIR POSIBILIDADES
+eliminar_repetidos([], []).
+eliminar_repetidos([H|T], R) :-
+    member(H, T), % Si H está en el resto de la lista, lo ignoramos
+    eliminar_repetidos(T, R).
+eliminar_repetidos([H|T], [H|R]) :-
+    \+ member(H, T), % Si H no está en el resto de la lista, lo mantenemos
+    eliminar_repetidos(T, R).
+
+unir(X,Y,L) :-
+    append(X,Y,ZR),
+    eliminar_repetidos(ZR,Z),
+    sort(Z,L).
