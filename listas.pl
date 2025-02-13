c7dba37768f1c4684655caf71184478e74c000e1
Cristian <cristimg9@gmail.com>
2025-02-13 09:23:16 +0100

    Prologogo

---
+++ listas.pl
@@ -0,0 +1,72 @@
+% LONGITUD BASIC
+longitud(0, []).
+longitud(Longitud, [_|Cola]) :- longitud(L,Cola), Longitud is L+1.
+
+% LONGITUD ACUMULADOR
+longitud_acu(Longitud, Acumulador, [_|Cola]) :- A is Acumulador+1, longitud_acu(Longitud, A, Cola).
+longitud_acu(Longitud, Longitud, []).
+longitud2(Longitud, Lista) :- longitud_acu(Longitud, 0, Lista).
+
+% MAXIMO
+max_acu(Maximo, Acumulador, [Cabeza|Cola]) :-
+    (Cabeza < Acumulador ->
+        max_acu(Maximo, Acumulador, Cola)
+    ;
+        max_acu(Maximo, Cabeza, Cola)
+    ).
+
+max_acu(Acumulador, Acumulador, []).
+max(Maximo, [Cabeza|Cola]) :- max_acu(Maximo, Cabeza, [Cabeza|Cola]).
+
+% MINIMO
+min_acu(Minimo, Acumulador, [Cabeza|Cola]) :-
+    (Cabeza > Acumulador ->
+        min_acu(Minimo, Acumulador, Cola)
+    ;
+        min_acu(Minimo, Cabeza, Cola)
+    ).
+
+min_acu(Acumulador, Acumulador, []).
+min(Minimo, [Cabeza|Cola]) :- min_acu(Minimo, Cabeza, [Cabeza|Cola]).
+
+% MIEMBRO
+miembro(X, [X|_]).
+miembro(X, [_|Cola]) :- miembro(X, Cola).
+
+% CHECK LISTAS EJEMPLOS
+igual([], []).
+igual([X|Cola1], [X|Cola2]) :- igual(Cola1, Cola2).
+
+% CONCATENA
+concatena([], L, L).
+concatena([Cabeza|Cola], L2, [Cabeza|L3]) :- concatena(Cola, L2, L3).
+
+% encuentra prefijos
+prefijo(Prefijo, L) :- concatena(Prefijo, _, L).
+
+% encuentra sufijos
+sufijo(Sufijo, L) :- concatena(_, Sufijo, L).
+
+% SUBLISTA: prefijo de un sufijo de una lista
+sublista(SubL, L) :- sufijo(Sufijo, L), prefijo(SubL, Sufijo).
+
+% INVIERTE NAIVE
+invierte_n([], []).
+invierte_n([Cabeza|Cola], Inv) :- invierte_n(Cola, ColaI), concatena(ColaI, [Cabeza], Inv).
+
+% INVIERTE
+invierte([], Acumulador, Acumulador).
+invierte([Cabeza|Cola], Acumulador, LInv) :- invierte(Cola, [Cabeza|Acumulador], LInv).
+
+% GET POS SIMPLE
+get([Cabeza|_], 0, Cabeza).
+get([_|Cola], Pos, Valor) :- P is Pos - 1, get(Cola, P, Valor).
+
+% GET POS + ENUM
+enum(Lista) :- enum(Lista, 0).
+
+enum([], _).
+enum([Cabeza|Cola], Indice) :-
+    write(Indice),  write(' - '), write(Cabeza), write('\n'),
+    Next is Indice + 1,
+    enum(Cola, Next).
---
+++ sudoku.pl
@@ -0,0 +1,29 @@
+% DEVOLVER FILA DE SUDOKU
+fila([A, B, C, D, E, F, G, H, I |_], 0, [A, B, C, D, E, F, G, H , I]).
+fila([_, _, _, _, _, _, _, _, _ | Cola], Pos, Fila) :-
+    P is Pos - 1,
+    fila(Cola, P, Fila).
+
+% DEVOLVER COLUMNA DE SUDOKU
+columna(Matriz, Pos, Columna) :-
+    columna_aux(Matriz, Pos, [], Columna).
+columna_aux(_, _, 0, Acumulador, Columna) :-
+    reverse(Acumulador, Columna).
+columna_aux(Matriz, Indice, Restantes, Acumulador, Columna) :-
+    nth0(Indice, Matriz, Elemento),
+    I is Indice + 9,
+    R is Restantes - 1,
+    columna_aux(Matriz, I, R, [Elemento|Acumulador], Columna).
+
+eliminar_repetidos([], []).
+eliminar_repetidos([H|T], R) :-
+    member(H, T), % Si H está en el resto de la lista, lo ignoramos
+    eliminar_repetidos(T, R).
+eliminar_repetidos([H|T], [H|R]) :-
+    \+ member(H, T), % Si H no está en el resto de la lista, lo mantenemos
+    eliminar_repetidos(T, R).
+
+unir_lista(X,Y,L) :-
+    append(X,Y,ZR),
+    eliminar_repetidos(ZR,Z),
+    sort(Z,L).
